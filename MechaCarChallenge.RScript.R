1e61be2aa340375b60b7726d3a73c01cb45d2a2a
ericaguti <ericagguti@gmail.com>
2022-11-27 13:40:37 -0800

    screenshots

---
+++ MechaCarChallenge.RScript.R
@@ -0,0 +1,13 @@
+library(dplyr)
+MechaCar<-read.csv('MechaCar_mpg.csv')
+lm(mpg~vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD ,data=MechaCar) 
+summary(lm(mpg~vehicle_length+vehicle_weight+spoiler_angle+ground_clearance+AWD, data=MechaCar))
+Suspension_Coil<-read.csv('Suspension_Coil.csv')
+total_summary <- Suspension_Coil  %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI))
+total_summary <- Suspension_Coil  %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI))
+lot_summary <- Suspension_Coil  %>% group_by(Manufacturing_Lot) %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI), .groups = 'keep')          
+t.test(Suspension_Coil $PSI, mu=1500)
+t.test(subset(subset(Suspension_Coil, Manufacturing_Lot== "Lot1"))$PSI, mu=1500)
+t.test(subset(subset(Suspension_Coil, Manufacturing_Lot== "Lot2"))$PSI, mu=1500)
+t.test(subset(subset(Suspension_Coil, Manufacturing_Lot== "Lot3"))$PSI, mu=1500)
+
